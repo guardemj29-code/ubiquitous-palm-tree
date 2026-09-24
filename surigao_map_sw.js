f345f9c222ad47ff2de16f4ae6f7e8093fef9ca2
guardemj29-code <guardemj29@gmail.com>
2026-09-24 14:09:17 +0800

    Add files via upload

---
+++ surigao_map_sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'ayo-surigao-map-v1';
+const SHELL = [
+  './surigao_map.html',
+  './surigao_map.webmanifest',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      if (response.ok && (event.request.url.includes('/surigao_map') || event.request.url.includes('unpkg.com'))) {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+      }
+      return response;
+    }).catch(() => caches.match('./surigao_map.html')))
+  );
+});
